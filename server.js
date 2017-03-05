97d851f5ade1ae10875c53188c2357c365e70853
Robert van der Mark <robert.vandermark@gmail.com>
2017-03-04 19:01:12 -0500

    capitalized PORT to use for Heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
 
 app.use(express.static(path.join(__dirname,'node_modules')));
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/',(req, res, next)=> {
 	db.model.User.findAll()
