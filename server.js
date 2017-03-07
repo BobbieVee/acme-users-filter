5d4ea9a0743d58e2a2ae71976d93f76d959346f6
Robert van der Mark <robert.vandermark@gmail.com>
2017-03-07 09:36:46 -0500

    broke app.js and serer.js apart

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-const express = require('express');
-const nunjucks = require('nunjucks');
-const app = express();
+const server = require('http').createServer(require('./app'));
 const db = require('./db');
-const path = require('path');
-const routes =  require("./routes.js")
-
 const port = process.env.PORT || 3000;
-const noCache = process.env.NOCACHE || false;
-
-app.set('view engine', 'html');
-app.engine('html', nunjucks.render);
-nunjucks.configure('views', {noCache: true});
-
-app.use(express.static(path.join(__dirname,'node_modules')));
-
-app.use('/', routes)
 
 db.seed()
-.then(()=> app.listen(port, ()=> console.log(`listening on port ${port}\n noCache = ${noCache}`)))
+.then(()=> server.listen(port, ()=> console.log(`listening on port ${port}\n`)))
 .catch( e => console.log(e));
 
